1573d7a719282c9b9f03afb5516da5bb257a9085
djbobbydrake <djbobbydrake@gmail.com>
2015-01-30 11:21:38 -0500

    Grab json from serbanghita's repo and print it.

--- mobile_detect.go
+++ mobile_detect.go
@@ -1,8 +1,32 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "net/http"
+  "io/ioutil"
+  "os"
+)
+
+const (
+  MOBILE_DETECT_JSON = "https://raw.githubusercontent.com/serbanghita/Mobile-Detect/master/Mobile_Detect.json"
+)
 
 func main() {
-  fmt.Printf("Hello, world.\n")
+  // Grab json file from github.
+  resp, err := http.Get(MOBILE_DETECT_JSON)
+  if err != nil {
+    fmt.Printf("%s", err)
+    os.Exit(1)
+  } else {
+    defer resp.Body.Close()
+    // Grab contents of json file.
+    contents, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+      fmt.Printf("%s", err)
+      os.Exit(1)
+    }
+    fmt.Printf("%s\n", string(contents))
+  }
 }
+
 
